23231d8133c3ccefa750bf1c32e1a5d171f0672a
Esteban Palomeque <shaggy.palomeque@gmai.com>
2022-05-16 15:56:05 -0500

    Extend tailwind config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     './layouts/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          400: '#a78bfa',
+          500: '#8b5cf6',
+          600: '#7c3aed',
+          700: '#6d28d9',
+        },
+        secondary: {
+          500: '#EE903A',
+          600: '#E57815',
+          700: '#D26E14',
+        },
+      },
+    },
   },
   plugins: [],
 }
