9534c2b01ebd9a61d9f3e555ff65970d436eff29
Esteban Palomeque <shaggy.palomeque@gmai.com>
2022-05-21 00:20:38 -0500

    Create survey page

--- pages/survey.js
+++ pages/survey.js
@@ -1,7 +1,21 @@
+import { useState } from 'react'
 import DefaultLayout from 'layouts/DefaultLayout'
+import Surveys from '@/components/Surveys'
+
+const questionInitialState = [{ id: 1, value: 0 }]
 
 export default function Survey() {
-  return <h1>The suvey page</h1>
+  const [surveyResults, setSurveyResults] = useState(questionInitialState)
+
+  return (
+    <div className="px-1 sm:px-4">
+      <h1 className="font-bold text-gray-600 text-2xl text-center mt-8">
+        Encuesta sobre la{' '}
+        <span className="text-secondary-500">Responsabilidad Social</span>
+      </h1>
+      <Surveys results={surveyResults} setResults={setSurveyResults} />
+    </div>
+  )
 }
 
 Survey.getLayout = function getLayout(page) {
